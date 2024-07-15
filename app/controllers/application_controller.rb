b0946cd96cbe7286a8d37befd59912561764cea3
aieest <aie.estidola@gmail.com>
2024-07-15 14:41:58 +1200

    Updated Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :redirect_based_on_auth
+
+  private
+
+  def redirect_based_on_auth
+    if user_signed_in?
+      redirect_to home_path unless current_page?(home_path)
+    else
+      redirect_to guest_path unless current_page?(guest_path)
+    end
+  end
 end
