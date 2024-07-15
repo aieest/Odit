e984490586b2f073f481441eb9b38edd91f58210
aieest <aie.estidola@gmail.com>
2024-07-15 14:41:18 +1200

    Cleaned and modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/guest'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  authenticated :user do
+    root 'pages#home', as: :authenticated_root
+  end
+  
+  root 'pages#guest'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get 'home', to: 'pages#home'
+  get 'guest', to: 'pages#guest'
+
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
