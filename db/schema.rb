b0724a50f712f8481059bb04dc25279a40611baf
aieest <aie.estidola@gmail.com>
2024-07-17 11:24:42 +1200

    Migrated Profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_15_022346) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_231256) do
+  create_table "profiles", force: :cascade do |t|
+    t.string "given_name"
+    t.string "last_name"
+    t.string "gender"
+    t.string "profile_picture"
+    t.decimal "balance"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +35,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_15_022346) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
