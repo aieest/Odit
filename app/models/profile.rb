6f3a8b09fbf6f17a21f8a22b9ffb6e668b1e84f2
aieest <aie.estidola@gmail.com>
2024-07-22 13:09:30 +1200

    Added profile completetion checker

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -8,4 +8,8 @@ class Profile < ApplicationRecord
   validates :given_name, :last_name, presence: true
   validates :gender, inclusion: { in: ['Male', 'Female'] }
   validates :balance, numericality: { greater_than_or_equal_to: 0 }
+
+  def complete?
+    given_name.present? && last_name.present? && gender.present? && balance.present?
+  end
 end
