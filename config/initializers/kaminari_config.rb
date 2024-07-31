8fe506f07658040ea103cb6b6309373c7a0c50d4
aieest <aie.estidola@gmail.com>
2024-07-31 18:06:40 +1200

    Updated pagination config and views

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,7 +1,6 @@
 Kaminari.configure do |config|
   config.default_per_page = 5
+  config.max_pages = 7
   config.window = 2
-  config.outer_window = 0
-  config.left = 1
-  config.right = 1
+  config.outer_window = 1
 end
